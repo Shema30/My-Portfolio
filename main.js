7ee9cdce730b8bbfacdecabab15a5dad8301a2e6
Shema <shemaserge23@gmail.com>
2024-05-08 17:29:08 +0200

    Switch themes button added

--- main.js
+++ main.js
@@ -3,4 +3,34 @@ function toggleMenu () {
     const icon = document.querySelector(".hamburger-icon")
     menu.classList.toggle("open")
     icon.classList.toggle("open")
+
+    // document.addEventListener("click", function (){
+    //     menu.classList.remove("open")
+    //     icon.classList.remove("open")
+    // })
 }
+
+// Switch to Both Dark and Light Theme 
+
+// Function to toggle between dark and light themes
+function toggleTheme() {
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme === 'dark') {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+}
+
+// Check for saved theme preference
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+    document.documentElement.setAttribute('data-theme', savedTheme);
+} else {
+    // Default to light theme if no preference is saved
+    document.documentElement.setAttribute('data-theme', 'light');
+}
+
+// Bouncing Button
